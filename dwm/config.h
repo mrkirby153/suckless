e577a2bbacd96f114ae44890ba82d6c16794a949
mrkirby153 <mr.austinwhyte@gmail.com>
2024-08-23 18:13:31 -0700

    Terminal shenanigans

--- dwm/config.h
+++ dwm/config.h
@@ -68,6 +68,7 @@ static const Rule rules[] = {
     {"Spotify",         	NULL,           NULL,       TAG_MUSIC_MASK, 	0,			0,          -1 },
 
 	{ "St",      			"st",     		NULL,       TAG_TERM_MASK,      1,			0,          -1 },
+    { "com.mitchellh.ghostty", "ghostty", NULL, TAG_TERM_MASK, 1, 0, -1 },
 
 	{"firefox",				NULL,			NULL,		TAG_BROWSER_MASK,	1,			0,			-1 },
 
@@ -112,7 +113,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL};
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "default-terminal", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", "-c", "scratchpad", NULL };
 
