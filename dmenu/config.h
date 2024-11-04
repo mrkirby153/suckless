e3db543ce27d38088fb2f1769b1ba8a901d85b74
mrkirby153 <mr.austinwhyte@gmail.com>
2024-11-03 16:52:11 -0800

    Update dmenu

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,22 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 750;                    /* minimum width when centered */
+static int topbar = 1;		/* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;	/* -c option; centers dmenu on screen */
+static int min_width = 750; /* minimum width when centered */
 static int fuzzy = 1;
+static const float menu_height_ratio = 4.0f; /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"SauceCodeProNF:size=10:antialias=true:autohint=true"};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = {"#bbbbbb", "#222222"},
+	[SchemeSel] = {"#eeeeee", "#005577"},
+	// [SchemeSelHighlight] = {"#ffc978", "#005577"},
+	// [SchemeNormHighlight] = {"#ffc978", "#222222"},
+	[SchemeOut] = {"#000000", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -24,4 +27,4 @@ static unsigned int lines      = 0;
  */
 static const char worddelimiters[] = " ";
 /* Size of the window border */
-static const unsigned int border_width = 2;
+static unsigned int border_width = 2;
